7ca1322548906f8343bca1e44c1eeeaf73f3a047
sh990920 <sh990920@gmail.com>
2023-08-01 17:23:55 +0900

    git ignore

--- src/main/java/com/yanolja/clone/lego/util/Type.java
+++
@@ -1,6 +0,0 @@
-package com.yanolja.clone.lego.util;
-
-public interface Type {
-    // 사진인지 아닌지 체크
-    String[] IMAGE_TYPE = {"jpg", "png", "jpeg", "gif", "bmp", "tiff", "psd", "tga", "ai", "svg", "img"};
-}
